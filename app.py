ba7fc3798d5a04c838afd1e4c9205d83da5467f9
Gabriela Vidales <158981222+GabrielaVidales@users.noreply.github.com>
2025-07-11 09:11:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,6 @@ def draw_2d():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     print(f"Inicializando en puerto: {port}")
     app.run(host='0.0.0.0', port=port, debug=True)
