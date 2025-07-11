43f523e0b3056358b11bacced02d67ff04a20ff8
Gabriela Vidales <158981222+GabrielaVidales@users.noreply.github.com>
2025-07-11 09:10:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def draw_2d():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+    print(f"Inicializando en puerto: {port}")
     app.run(host='0.0.0.0', port=port, debug=True)
