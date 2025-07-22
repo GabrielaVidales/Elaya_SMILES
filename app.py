f01deb45808b80db66d42152d2a73dfc22192d00
Gabriela Vidales <158981222+GabrielaVidales@users.noreply.github.com>
2025-07-22 15:44:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,6 +98,4 @@ def draw_2d():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    print(f"Inicializando en puerto: {port}")
-    app.run(host='0.0.0.0', port=port)  # debug removido para producción
+    app.run()  # debug removido para producción
